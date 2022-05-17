e4904682814d84eedfb99ef831bb8642edbbf010
pbenderson <souza98120@gmail.com>
2022-05-17 09:06:06 -0300

    .

--- agenda.java
+++ agenda.java
@@ -1,36 +1,39 @@
 public class agenda {
-  
-    private String nome;
-    private String idade;
-    private String filme;
-    
-    //parseInt Idade
-    public agenda(String entrada) {
-        
-        String[] entradaA = entrada.split(";", 3);
 
-        this.nome = entradaA[0];
-        this.idade = entradaA[1];
-        this.filme = entradaA[2];
-    }
-    public String getNome() {
-        return nome;
-    }
-    public void setNome(String nome) {
-        this.nome = nome;
-    }
-    public String getIdade() {
-        return idade;
-    }
-    public void setIdade(String idade) {
-        this.idade = idade;
-    }
-    public String getFilme() {
-        return filme;
-    }
-    public void setFilme(String filme) {
-        this.filme = filme;
-    }
+  private String nome;
+  private String idade;
+  private String filme;
 
-    
+  //parseInt Idade
+  public agenda(String entrada) {
+    String[] entradaA = entrada.split(";", 3);
+
+    this.nome = entradaA[0];
+    this.idade = entradaA[1];
+    this.filme = entradaA[2];
+  }
+
+  public String getNome() {
+    return nome;
+  }
+
+  public void setNome(String nome) {
+    this.nome = nome;
+  }
+
+  public String getIdade() {
+    return idade;
+  }
+
+  public void setIdade(String idade) {
+    this.idade = idade;
+  }
+
+  public String getFilme() {
+    return filme;
+  }
+
+  public void setFilme(String filme) {
+    this.filme = filme;
+  }
 }
--- teste.java
+++ teste.java
@@ -1,56 +1,64 @@
-import java.util.Scanner;
 import java.util.ArrayList;
-import java.util.Collection;
+//import java.util.Collection;
 import java.util.List;
+import java.util.Scanner;
 
 public class teste {
-    public static void main(String[] args) {
-      Scanner scanner = new Scanner(System.in);
-      Scanner leitores = new Scanner(System.in);
-  
 
-        ArrayList<cinema> listaCinema = new ArrayList<cinema>();
-         System.out.println("infome quantidade de salas do cinema ");
-        int variSala = scanner.nextInt();
-        for(int i=0; i<variSala; i++){
-            System.out.printf("Sala %d - Digite suas informações " + "Nome Filme, Faixa Etaria, Capacidade sala",(i+1));
-            String pessoaCinema = leitores.nextLine();
-            cinema saidaPessoaCinema = new cinema(pessoaCinema);
-            
-            // adicionando dentro da lista "SALA"
-            listaCinema.add(saidaPessoaCinema);
-        }
-            
-        ArrayList<agenda> ListaAgendamento = new ArrayList<agenda>();
-        
-        System.out.println("Informe Número de agendamentos ");
-        int vari = scanner.nextInt();
-        //Parte agendamento
-        //nome, idade, filme
-        for(int i=0; i<vari; i++){
-            System.out.printf("Agendamento %d - Digite suas informações " + "Nome, Idade, filme",(i+1));
-            String frase = leitores.nextLine();
-            
-              agenda a =  new agenda(frase);
+  public static void main(String[] args) {
+    Scanner scanner = new Scanner(System.in);
+    Scanner leitores = new Scanner(System.in);
 
-            ListaAgendamento.add(a);
-        }
+    ArrayList<cinema> listaCinema = new ArrayList<cinema>();
+    System.out.println("infome quantidade de salas do cinema ");
+    int variSala = scanner.nextInt();
+    for (int i = 0; i < variSala; i++) {
+      System.out.printf(
+        "Sala %d - Digite suas informações " +
+        "Nome Filme, Faixa Etaria, Capacidade sala",
+        (i + 1)
+      );
+      String pessoaCinema = leitores.nextLine();
+      cinema saidaPessoaCinema = new cinema(pessoaCinema);
 
-        //SAIDA
-        for(agenda saidaAgendamento : ListaAgendamento){
-            System.out.println("Nome: " + saidaAgendamento.getNome());
-            System.out.println("Idade: " + saidaAgendamento.getIdade());
-            System.out.println("Filme: " + saidaAgendamento.getFilme());
-            
-        }
+      // adicionando dentro da lista "SALA"
+      listaCinema.add(saidaPessoaCinema);
+    }
 
-        System.out.println("\n\n");
+    ArrayList<agenda> ListaAgendamento = new ArrayList<agenda>();
 
-        for(cinema saidafilme : listaCinema){
-            System.out.println("Nome Filme: " + saidafilme.getnomefilme());
-            System.out.println("Faixa Etaria: " + saidafilme.getfaixaetaria() );
-            System.out.println("Capacidade Sala: " + saidafilme.getcapacidadeSala());
-            
-        }
-    }   
-}   
+    System.out.println("Informe Número de agendamentos ");
+    int vari = scanner.nextInt();
+    //Parte agendamento
+    //nome, idade, filme
+    for (int i = 0; i < vari; i++) {
+      System.out.printf(
+        "Agendamento %d - Digite suas informações " + "Nome, Idade, filme",
+        (i + 1)
+      );
+      String frase = leitores.nextLine();
+
+      agenda a = new agenda(frase);
+
+      ListaAgendamento.add(a);
+    }
+
+    //SAIDA
+    //Incompleto
+    for (agenda saidaAgendamento : ListaAgendamento) {
+      System.out.println("Nome: " + saidaAgendamento.getNome());
+      System.out.println("Idade: " + saidaAgendamento.getIdade());
+      System.out.println("Filme: " + saidaAgendamento.getFilme());
+    }
+
+    System.out.println("\n");
+
+    for (cinema saidafilme : listaCinema) {
+      System.out.println("Nome Filme: " + saidafilme.getnomefilme());
+      System.out.println("Faixa Etaria: " + saidafilme.getfaixaetaria());
+      System.out.println("Capacidade Sala: " + saidafilme.getcapacidadeSala());
+    }
+
+    System.out.println("\n");
+  }
+}
